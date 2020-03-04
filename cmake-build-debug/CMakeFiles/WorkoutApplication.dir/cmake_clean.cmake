20b02dbf02c5d876ce65418bce3ff9739221824a
James Jacobson <james.jacobson@snhu.edu>
2020-03-04 12:30:09 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/WorkoutApplication.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WorkoutApplication.dir/main.cpp.obj"
+  "WorkoutApplication.exe"
+  "WorkoutApplication.exe.manifest"
+  "WorkoutApplication.pdb"
+  "libWorkoutApplication.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WorkoutApplication.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
